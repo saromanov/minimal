22cd5650fffb1c63a46bf0ea20e306fd0dcaa6f8
Sergey <xxsmotur@gmail.com>
2018-04-22 17:27:38 +0500

    Add more fields into comment body

--- db/model/author.go
+++ db/model/author.go
@@ -1,7 +1,10 @@
 package model
 
+import "github.com/satori/go.uuid"
+
 // Author provides definition for authour struct
 type Author struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	Id          uuid.UUID `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
 }
--- db/model/comment.go
+++ db/model/comment.go
@@ -1,6 +1,10 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/satori/go.uuid"
+)
 
 // Comment provides comment body
 type Comment struct {
@@ -8,4 +12,8 @@ type Comment struct {
 	Body string `json:"body"`
 	// Created provides creating time for comment
 	CreatedAt time.Time `json:"createdAt"`
+	// AuthorID provides author of comment
+	AuthorID uuid.UUID `json:"id"`
+	// mentions provides definitions of mentions for comment
+	Mentions []uuid.UUID `json:"mentions"`
 }
