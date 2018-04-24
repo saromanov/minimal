fc9224d1984e7aee79c1f8466c2d8e56a665c992
Sergey <xxsmotur@gmail.com>
2018-04-24 10:47:46 +0500

    Extending of author table

--- db/model/author.go
+++ db/model/author.go
@@ -7,4 +7,6 @@ type Author struct {
 	ID          uuid.UUID `json:"id"`
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
+	Link        string    `json:"link"`
+	JoinedAt    string    `json:"joinedAt"`
 }
